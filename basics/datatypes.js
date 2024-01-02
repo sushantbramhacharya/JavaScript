17bee56ef983e04779265fff72fc0588e1a7f776
sushantbramhacharya <sushantbramacharya@gmail.com>
2024-01-02 20:47:15 +0545

    datatypes overview js

---
+++ basics/datatypes.js
@@ -0,0 +1,27 @@
+//Different data types in js
+let Numbers=10
+let boolValues=true
+let bigNumber=1231235152n
+let string="String Data Type"
+let nullData=null
+let dataNotDefined
+
+console.log(typeof Numbers);//number
+console.log(typeof boolValues);//boolean
+console.log(typeof bigNumber);//bigInt
+console.log(typeof string);//undefined
+console.log(typeof nullData);//object
+console.log(typeof dataNotDefined);//undefined
+
+//We can declare explicitly
+let num=Number("10")//Automatically convert into number
+let num2=Number("10"+2)//returns 102 becuase treats as string then casts to number
+let num3=Number(10+"2")//returns 102 becuase treats as string then casts to number
+let str=String("Hello World")
+let bigNum=BigInt(1232131234)
+
+console.log(num+10);
+console.log(num2);
+console.log(num3);
+console.log(str);
+console.log(bigNum);
