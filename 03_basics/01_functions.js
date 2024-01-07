bb6a14a2a215b34ceef6e1ec534f3d833a25bfe7
sushantbramhacharya <sushantbramacharya@gmail.com>
2024-01-07 10:26:05 +0545

    func 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,3 +9,27 @@ function loginUserMsg(username="Sam")//Default Args
 }
 // console.log(loginUserMsg("sush"))
 // console.log(loginUserMsg())//returns undefined if doesnt give param
+
+function calculateCartPrice(val1,val2,...num1)//Rest Operator can accept multiple args
+{
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500,800,1000))//200 and 400 goes to val1 and val2
+
+const user={
+    username:"sush",
+    ages:22
+}
+
+function handleObject(anyobj)
+{
+    console.log(`Username is ${anyobj.username} age is ${anyobj.age}`)
+
+}
+
+// handleObject(user)
+handleObject({
+    username:"sam",
+    age:19
+})
